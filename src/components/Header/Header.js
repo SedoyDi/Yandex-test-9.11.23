a99225a37d9d3aedabeaecdab85b14c30cef6100
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2023-11-11 17:00:54 +0300

    feet: add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+import image from '../../images/header-img.png';
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <div className='header__container'>
+                <div className="header__content">
+                    <h1 className='header__title'>Сезон согревающих напитков</h1>
+                    <p className='header__discription'>Подборка кофемашин и подходящего кофе</p>
+                </div>
+                <img src={image} alt='кофеварка' className='header__img' />
+            </div>
+        </header>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -8,4 +8,3 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />);
-
